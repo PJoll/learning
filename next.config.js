408a2ecfa046134a1f4238235fc0beb65cec19c0
Peter Jolley <peter@marango.com.au>
2023-06-01 11:43:22 +0930

    server set up env set up pusher env set up for app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+    /* next.config.js */
+    
+    const webpack = require('webpack');
+    require('dotenv').config();
+    
+    module.exports = {
+      webpack: config => {
+        const env = Object.keys(process.env).reduce((acc, curr) => {
+          acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+          return acc;
+        }, {});
+        
+        config.plugins.push(new webpack.DefinePlugin(env));
+        
+        return config;
+      }
+    };
